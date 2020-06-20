380c7913c2cb62879601bba6b7bf766db51968bd
Purple <2569677439@qq.com>
2020-06-20 19:30:32 +0800

    Commit Message

--- src/Learning/GroupTest.java
+++ src/Learning/GroupTest.java
@@ -1,4 +1,12 @@
 package Learning;
 
+import java.util.Objects;
+
 public class GroupTest {
+    public static void main(String[] args) {
+        int[] group = new int[6];
+        String stringtest = "test";
+        StringBuffer stringBuffer = new StringBuffer(stringtest);
+        System.out.println(stringBuffer.reverse());
+    }
 }
